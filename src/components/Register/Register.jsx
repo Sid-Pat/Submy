56513b7ad620e36acb069fe4abdf15c8f0b33683
Sid-Pat <siddhantujjain123@gmail.com>
2024-04-15 19:16:13 +0530

    team logic improved database and render part

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -8,12 +8,11 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { auth, db } from '../../config/firebase'
+import { auth } from '../../config/firebase'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { useContext } from 'react';
 import UserContext from '../../context/UserContext';
 import { styled } from '@mui/material/styles';
-import { addDoc, collection } from 'firebase/firestore';
 import { Navigate } from 'react-router-dom';
 
 const Div = styled('div')(({ theme }) => ({
@@ -21,8 +20,6 @@ const Div = styled('div')(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
   padding: theme.spacing(1),
 }));
-
-const teamsCollectionRef =  collection(db , "teams");
 
 function Copyright(props) {
   return (
@@ -52,26 +49,6 @@ export default function SignIn() {
             console.log(err)
         }
     };
-
-    const createTeam = async({...team}) => {
-        const s1=team.s1sid+'('+team.s1name+')';
-        const s2=team.s2sid+'('+team.s2name+')';
-        const s3=team.s3sid+'('+team.s3name+')';
-        const s4=team.s4sid+'('+team.s4name+')';
-        const studentArray = [s1,s2,s3,s4];
-        console.log("Team Registering  ...")
-        try{
-            await addDoc(teamsCollectionRef, {
-              email:team.email,
-              project:team.project,
-              teamInfo:studentArray,
-            });
-        }catch(err){
-            console.log(err);
-        }
-        console.log("Team Registering  ...")
-    }
-
     const handleSubmit = (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
@@ -82,20 +59,6 @@ export default function SignIn() {
         const {email,password} = {email:data.get('email'),
         password:data.get('password')}
         createUser(email,password);
-        console.log("Create Team")
-        const {...teamInfo} = {
-            email:data.get('email'),
-            s1sid:data.get('s1sid'),
-            s1name:data.get('s1name'),
-            s2sid:data.get('s2sid'),
-            s2name:data.get('s2name'),
-            s3sid:data.get('s3sid'),
-            s3name:data.get('s3name'),
-            s4sid:data.get('s4sid'),
-            s4name:data.get('s4name'),
-            project:data.get('project'),
-        }
-        createTeam(teamInfo)
     };
   if(loggedIn){
     return <Navigate to="/team" />
@@ -140,88 +103,6 @@ export default function SignIn() {
               id="password"
               sx={{mb:4}}
               autoComplete="current-password"
-            />
-            <Div>Team Registration</Div>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="s1sid"
-              label="Student 1 Scholar Number(Team Representative)"
-              id="s1sid"
-              autoComplete="s1sid"
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="s1name"
-              label="Student 1 Name(Team Representative)"
-              id="s1name"
-              autoComplete="s1name"
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="s2sid"
-              label="Student 2 Scholar Number"
-              id="s2sid"
-              autoComplete="s2sid"
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="s2name"
-              label="Student 2 Name"
-              id="s2name"
-              autoComplete="s2name"
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="s3sid"
-              label="Student 3 Scholar Number"
-              id="s3sid"
-              autoComplete="s3sid"
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="s3name"
-              label="Student 3 Name"
-              id="s3name"
-              autoComplete="s3name"
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="s4sid"
-              label="Student 4 Scholar Number"
-              id="s4sid"
-              autoComplete="s4sid"
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="s4name"
-              label="Student 4 Name"
-              id="s4name"
-              autoComplete="s4name"
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="project"
-              label="Project Name"
-              id="project"
-              autoComplete="project"
             />
             <Button
               type="submit"
--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,33 +1,264 @@
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { auth, db } from '../../config/firebase'
+// import { useContext } from 'react';
+// import UserContext from '../../context/UserContext';
+import { styled } from '@mui/material/styles';
+import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import { useEffect , useState} from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Diversity3Icon from '@mui/icons-material/Diversity3';
+import WorkIcon from '@mui/icons-material/Work';
+// import { Navigate } from 'react-router-dom';
 
-export default function Team() {
+const Div = styled('div')(({ theme }) => ({
+  ...theme.typography.button,
+  backgroundColor: theme.palette.background.paper,
+  padding: theme.spacing(1),
+}));
+
+const teamsCollectionRef =  collection(db , "teams");
+
+function Copyright(props) {
   return (
-    <Box sx={{  marginTop: 8,
-        marginBottom: 4,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center', width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      <Divider />
-      <nav aria-label="secondary mailbox folders">
-        <List>
-          <ListItem disablePadding>
-            <ListItemButton>
-              Hello
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="#simple-list">
-              <ListItemText primary="Spam" />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      <Divider />
-      </nav>
-    </Box>
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Submy
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
+
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+export default function Team() {
+    const [team, setTeam] = useState([]);
+    
+    // const {loggedIn,setLoggedIn} = useContext(UserContext);
+    const getTeamList = async () => {
+      try{
+        // const data  = await getDocs(teamsCollectionRef)
+        // const filteredData = data.docs.map((doc)=> ({
+        //   ...doc.data(),
+        //   id:doc.id})
+        // );
+        const q1 = query(teamsCollectionRef, where("email","==",auth.currentUser.email));
+        const data = await getDocs(q1);
+        // data.forEach((doc)=>{
+        //   console.log(doc.id,"->",doc.data())
+        // })
+        if(data.empty){
+          setTeam([]);
+        }else{
+          setTeam(data.docs[0].data())
+        }
+
+        console.log(data.docs[0].data())
+        // console.log(filteredData);
+        // setMovieList(filteredData)
+      }catch(err){
+        console.log(err);
+      }
+    }
+    useEffect(()=>{
+      getTeamList();
+    },[])
+
+    const createTeam = async({...team}) => {
+        const email=auth.currentUser.email;
+        try{
+            await addDoc(teamsCollectionRef, {
+              email:email,
+              project:team.project,
+              teamInfo:{
+                [team.s1sid]:{
+                  name:team.s1name
+                },
+                [team.s2sid]:{
+                  name:team.s2name
+                },
+                [team.s3sid]:{
+                  name:team.s3name
+                },
+                [team.s4sid]:{
+                  name:team.s4name
+                },
+              },
+            });
+            getTeamList()
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const {...teamInfo} = {
+            s1sid:data.get('s1sid'),
+            s1name:data.get('s1name'),
+            s2sid:data.get('s2sid'),
+            s2name:data.get('s2name'),
+            s3sid:data.get('s3sid'),
+            s3name:data.get('s3name'),
+            s4sid:data.get('s4sid'),
+            s4name:data.get('s4name'),
+            project:data.get('project'),
+        }
+        createTeam(teamInfo)
+    };
+  if(team.length!==0){
+    return(
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <Div> <Diversity3Icon/> Team Info </Div>
+      {
+        Object.entries(team.teamInfo).map(([key,value],i)=>{
+          return(
+            <ListItem key={key}>
+            <ListItemAvatar>
+              <Avatar>
+                <WorkIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={value.name} secondary={key} />
+          </ListItem>
+          )
+        })
+      }
+    </List>
+    )
+  }
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'warning.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            New User
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 4 }}>
+            <Div>Team Registration</Div>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="s1sid"
+              label="Student 1 Scholar Number(Team Representative)"
+              id="s1sid"
+              autoComplete="s1sid"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="s1name"
+              label="Student 1 Name(Team Representative)"
+              id="s1name"
+              autoComplete="s1name"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="s2sid"
+              label="Student 2 Scholar Number"
+              id="s2sid"
+              autoComplete="s2sid"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="s2name"
+              label="Student 2 Name"
+              id="s2name"
+              autoComplete="s2name"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="s3sid"
+              label="Student 3 Scholar Number"
+              id="s3sid"
+              autoComplete="s3sid"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="s3name"
+              label="Student 3 Name"
+              id="s3name"
+              autoComplete="s3name"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="s4sid"
+              label="Student 4 Scholar Number"
+              id="s4sid"
+              autoComplete="s4sid"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="s4name"
+              label="Student 4 Name"
+              id="s4name"
+              autoComplete="s4name"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="project"
+              label="Project Name"
+              id="project"
+              autoComplete="project"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="warning"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Register
+            </Button>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
