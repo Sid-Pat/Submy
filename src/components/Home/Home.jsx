1444c7d08e8bf6fc954e39a116faf6154751788f
Sid-Pat <siddhantujjain123@gmail.com>
2024-04-19 23:21:39 +0530

    image changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
                   <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
                       <h2 className="text-4xl font-bold sm:text-5xl">
                           Hi User !
-                          <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
+                          {/* <span className="hidden sm:block text-4xl"></span> */}
                       </h2>
 
                       <Link
@@ -52,12 +52,12 @@ export default function Home() {
               </div>
             
               <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                  <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                  <img className="w-96" src="../../public/teamphoto.png!w700wp" alt="image1" />
               </div>
           </aside>
 
           <div className="grid  place-items-center sm:mt-20">
-              <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+              <img className="sm:w-96 w-48" src="../../public/project.jpeg" alt="image2" />
           </div>
 
           <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Add more ...</h1>
