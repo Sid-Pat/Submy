4bc2d36a2117a0a11772ba2df33888b55ce2db2f
Sid-Pat <siddhantujjain123@gmail.com>
2024-04-19 19:21:19 +0530

    header tour added and title improved.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,10 @@ import {Link,NavLink} from 'react-router-dom';
 import UserContext from '../../context/UserContext';
 import { signOut } from 'firebase/auth';
 import { auth } from '../../config/firebase';
+import React, { useRef, useState } from 'react';
+import { Tour } from 'antd';
+import { Typography } from '@mui/material';
+import BuildCircleIcon from '@mui/icons-material/BuildCircle';
 
 export default function Header() {
 const {loggedIn,setLoggedIn,setTeam} = useContext(UserContext);
@@ -17,16 +21,54 @@ const logOut = async () => {
     }
     console.log(auth.currentUser)
 }
+
+const ref1 = useRef(null);
+const ref2 = useRef(null);
+const ref3 = useRef(null);
+const ref4 = useRef(null);
+const ref5 = useRef(null);
+const ref6 = useRef(null);
+const [open, setOpen] = useState(false);
+const steps = [
+{
+    title: 'Homepage',
+    description: 'Contains all the access points.',
+    target: () => ref1.current,
+},
+{
+    title: 'About us',
+    description: 'About us and the project "Submy".',
+    target: () => ref2.current,
+},
+{
+    title: 'Suggestions for us',
+    description: 'Send us any suggestions or feature required. We always are quick to check :)',
+    target: () => ref3.current,
+},
+{
+    title: 'Create or view team',
+    description: 'First login to access this feature.',
+    target: () => ref4.current,
+},
+{
+    title: 'Submit or view project status',
+    description: 'Login required to access this feature. See your marks, comments or suggestions if any.',
+    target: () => ref5.current,
+},
+{
+    title: 'View all projects',
+    description: 'Look what others have done and view their projects.',
+    target: () => ref6.current,
+},
+];
+
+
   return (
       <header className="shadow sticky z-50 top-0">
           <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
               <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                   <Link to="/" className="flex items-center">
-                      <img
-                          src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                          className="mr-3 h-12"
-                          alt="Logo"
-                      />
+                    <Typography variant="h1" className='text-orange-700'>Submy <BuildCircleIcon sx={{transform:"scale(2.5)" }}/></Typography>
                   </Link>
 
                   <div className="flex items-center lg:order-2">
@@ -48,8 +90,9 @@ const logOut = async () => {
                       <Link
                           to="#"
                           className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                          onClick={()=>setOpen(true)}
                       >
-                          Get started
+                          Tour
                       </Link>
                   </div>
                   <div
@@ -59,6 +102,7 @@ const logOut = async () => {
                       <ul className="flex flex-row mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                           <li className='flex flex-row space-x-4'>
                               <NavLink
+                                  ref={ref1}
                                   to='/'
                                   className={({isActive}) =>
                                       `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive?"text-orange-700":"text-grey-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
@@ -67,6 +111,7 @@ const logOut = async () => {
                                   Home
                               </NavLink>
                               <NavLink
+                                  ref={ref2}
                                   to='/about'
                                   className={({isActive}) =>
                                       `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive?"text-orange-700":"text-grey-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
@@ -76,6 +121,7 @@ const logOut = async () => {
                               </NavLink>
                               <NavLink
                                   to='/contact'
+                                  ref={ref3}
                                   className={({isActive}) =>
                                       `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive?"text-orange-700":"text-grey-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                   }
@@ -84,6 +130,7 @@ const logOut = async () => {
                               </NavLink>
                               <NavLink
                                   to="/team"
+                                  ref={ref4}
                                   className={({isActive}) =>
                                       `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive?"text-orange-700":"text-grey-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                   }
@@ -92,6 +139,7 @@ const logOut = async () => {
                               </NavLink>
                               <NavLink
                                   to='/project'
+                                  ref={ref5}
                                   className={({isActive}) =>
                                       `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive?"text-orange-700":"text-grey-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                   }
@@ -100,6 +148,7 @@ const logOut = async () => {
                               </NavLink>
                               <NavLink
                                   to='/projectlist'
+                                  ref={ref6}
                                   className={({isActive}) =>
                                       `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive?"text-orange-700":"text-grey-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                   }
@@ -113,6 +162,16 @@ const logOut = async () => {
                   </div>
               </div>
           </nav>
+          <Tour
+        open={open}
+        onClose={() => setOpen(false)}
+        steps={steps}
+        indicatorsRender={(current, total) => (
+          <span>
+            {current + 1} / {total}
+          </span>
+        )}
+      />
       </header>
   );
 }
